d6ead76c57d55705b226dbb9864092422777d5d6
artistic_mammoth <thrl0ad@ya.ru>
2023-08-12 19:35:41 +0300

    setup xcode project

---
+++ FoodRunnerApp/FoodRunnerApp/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FoodRunnerApp
+//
+//  Created by Михайлов Александр on 12.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
